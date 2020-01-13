256a60f1026be06e0a2a7378b8786ea8522f5f87
mutali57 <52873748+mutali57@users.noreply.github.com>
2020-01-13 13:54:42 +0200

    latest commit

--- js/dash.js
+++ js/dash.js
@@ -1,12 +1,12 @@
 //var activeId=0;
 
-var a= [ "Introduction","Site Appreciation","Energy Yield Assessment",	
-	"Site Conditions and Turbine Suitability",	
-	"Contract Overview",	
+var a= [ "Introduction","Site Appreciation","Energy Yield Assessment",
+	"Site Conditions and Turbine Suitability",
+	"Contract Overview",
 	"Programme",
-	"OEM and Turbine Technology",	
+	"OEM and Turbine Technology",
 	"Balance of Plant Track Record",
-	"Supply - Turbine Supply Agreement",	
+	"Supply - Turbine Supply Agreement",
 	"Supply - Balance of Plant",
 	"Supply - Project Management Agreement",
 	"Supply - Interface",
@@ -20,8 +20,8 @@ var a= [ "Introduction","Site Appreciation","Energy Yield Assessment",
 	"Environmental",
 	"Permitting",
 	"Operational Performance",
-	"Financial Model"]; 
-	
+	"Financial Model"];
+
 	function topic(name,id,totalRisk){
 		//alert(totalRisk);
 		 var energyRisk=totalRisk;
@@ -42,14 +42,14 @@ var critical=0;
 	  critical++;
   }
   if (risk(my [x])=="L"){
-	  
+
 	  low++;
   }
   if (risk(my [x])=="M"){
 	  medium++;
   }
  }
-		
+
 		var ref="";
 	var textStr="";
   var num=id+1;
@@ -61,7 +61,7 @@ var critical=0;
 			"data-id='" + num + "'>" +
 			"<span class='glyphicon glyphicon-edit' />" +
 			"</button></td>" +
-	
+
 	  "<td>"+name+"</td>"+
 	  "<td></td>"+
 	  "<td></td>"+
@@ -72,11 +72,11 @@ var critical=0;
 	  "<td>"+critical+"</td></tr>";
 	  textStr=  $('tbody').append(ref);
 
-		
+
 	}
-	
-	
-	
+
+
+
 function asserts (ctl){
 	editRow = $(ctl).parents("tr");
 
@@ -84,29 +84,29 @@ function asserts (ctl){
 	alert($($(cols[0]).children('button')[0]).data ('id'));
 	var activeId = $($(cols[0]).children("button")[0]).data("id");
 	alert(activeId);
-	
-	
+
+
 		//window.location.href = 'energy.html';
 
-	
+
 }
- 
+
 function riskCounter (){
 alert (valList[0]);
- 
+
  //alert(valList.length);
  //topic(a [3],4,valList [1]);
  topic(a [2],3, valList [0]);
  topic(a[3],4, valList[1]);
- 
-	
-	
-	
-	  
-	  
+
+
+
+
+
+
 }
 
- 
+
 
 
 $(document).ready(function (){
@@ -115,10 +115,10 @@ fireInit();
 //readData();
 valRead('totalRisk');
 //valRead('totalRisk');
-	
-	  
-	  
 
-	
+
+
+
+
 	 //
 });
