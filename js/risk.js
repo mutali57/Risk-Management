bbeafcbb9069ed7ada3e5f8950dcfc25cf93126d
mutali57 <52873748+mutali57@users.noreply.github.com>
2020-01-14 13:40:15 +0200

    second day

--- js/risk.js
+++ js/risk.js
@@ -1,6 +1,6 @@
 
 function risk(score) {
-	
+
 	var  result ="";
 	var str= ["N","L","L","M","M","O","H","O","O","C"];
     ;
@@ -8,19 +8,19 @@ function risk(score) {
 	if (str[score] == "H")
 	{
 		result = "H";
-		
+
 	}
 	else if (str[score] == "M")
 	{
 		result = "M";
- 
+
 	}
 	else if (str[score] == "L")
 	{
 		result = "L";
- 
+
 	}
-	else if (str[score] == "C")  
+	else if (str[score] == "C")
 	{
 		result = "C";
 
@@ -31,36 +31,39 @@ function risk(score) {
 		result = "N";
 
 	}
-	
+
 	return result;
 }
 
 function riskScore(){
-	
-	
+
+
 	var serverityRisk;
 
 
 	if (valList[4] == "Low")
 	{
 		serverityRisk = 1;
-	
+
 	}
 	else if (valList[4] == "Medium")
 	{
 
 		serverityRisk = 2;
-		
+
 	}
 	else if (valList[4] == "High")
 	{
-		
+
 		serverityRisk = 3;
 	}
 	else if (valList[4] == "Negligible")
 	{
 		serverityRisk = 0;
-		
+
+	}
+	else {
+		serverityRisk=null;
 	}
 
 	var ProbRisk;
@@ -68,31 +71,29 @@ function riskScore(){
 	if (valList[6] == "Low")
 	{
 		ProbRisk = 1;
-		
+
 	}
 	else if (valList[6] == "Medium")
 	{
 
 		ProbRisk = 2;
-		
+
 	}
 	else if (valList[6] == "High")
 	{
 
 		ProbRisk = 3;
-		
+
 	}
 	else if (valList[6] == "Negligible")
 	{
 		ProbRisk = 0;
-		
-		
+
+
+	}
+	else {
+		ProbRisk=null;
 	}
 
 	 return ProbRisk * serverityRisk;
 }
-
-
-	
-	
-
