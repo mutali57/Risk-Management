570e99698f483c8fbc306d00af1215437ef4f845
mutali57 <mutali.nepfumbada@gmail.com>
2020-01-24 09:40:54 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,23 +2,8 @@ var valList= [];
 var keyList =[];
 var snapData;
 var RiskArray= [];
-//var databaseRes;
-function fireInit () {
-	var  a=  {
-
-		apiKey: "AIzaSyB5F9qY38724px4bwXULcrCwj8fCq_D58Y",
-		authDomain: "projectrisk-1a0a1.firebaseapp.com",
-		databaseURL: "https://projectrisk-1a0a1.firebaseio.com",
-		projectId: "projectrisk-1a0a1",
-		storageBucket: "projectrisk-1a0a1.appspot.com",
-		messagingSenderId: "659506389580",
-		appId: "1:659506389580:web:029194abbba2619576cae9"
-	};
-	 firebase.initializeApp(a);
 
 
-
-}
 
 function valRead(name) {
 
